4b9828a004baf32f211aee1e6bccfe81ab7f8eef
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-21 16:00:59 +0300

    Add files via upload
    полностью переделал, больше нет getDocument...

--- test.js
+++ test.js
@@ -1,4 +1,28 @@
-function delay() {
-  alert('Привет');
+var div, body, p, script;
+div = document.querySelectorAll('div');
+
+  for (var i = 0; i < div.length; i++) {
+	alert("тэгов div : " + div.length); 
+	break;
+}
+
+body = document.querySelectorAll('body');
+
+  for (var i = 0; i < body.length; i++) {
+	alert("тэгов body : " + body.length); 
+	break;
   }
-  setTimeout(delay, 2000);
+  
+p = document.querySelectorAll('p');
+
+  for (var i = 0; i < p.length; i++) {
+	alert("тэгов p : " + p.length); 
+	break;
+  }
+
+script = document.querySelectorAll('script');
+
+  for (var i = 0; i < script.length; i++) {
+	alert("тэгов script : " + script.length); 
+	break;
+  }
